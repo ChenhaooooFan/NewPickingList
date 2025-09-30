b9cfc381f2214a205e744140963f4f8a44a5affe
Chenhao Fan <146764940+ChenhaooooFan@users.noreply.github.com>
2025-09-30 10:03:04 -0700

    app.py

--- app.py
+++ app.py
@@ -99,7 +99,10 @@ sku_prefix_to_name = {
     "NOX007": "Stellar Whisper",
     "NOF014": "Desert Rose",
     "NPF019": "Lunar Whisper",
-    "NPJ003": "Stellar Capricorn"
+    "NPF020": "Mocha Grace",
+    "NOX009": "Moonlit Petal",
+    "NOX008": "Espresso Petals",
+    "NPX018": "Ruby Ribbon"
 
 }
 
