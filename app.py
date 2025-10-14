4a6d496b05b145098e41d7037bb947325deef0ab
Chenhao Fan <146764940+ChenhaooooFan@users.noreply.github.com>
2025-10-13 18:39:23 -0700

    app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ sku_prefix_to_name = {
     "NPX015":"Spooky Clown","NOX005":"Honey Daisy","NPJ012":"Gothic Mirage","NOX006":"Imperial Bloom","NPX017":"Rouge Letter",
     "NOF013":"Sakura Blush","NPF018":"Wild Berry","NOF012":"Rose Nocturne","NIX001":"Golden Maple","NOX007":"Stellar Whisper",
     "NOF014":"Desert Rose","NPF019":"Lunar Whisper","NOF015":"Mocha Grace","NOX009":"Moonlit Petal","NOX008":"Espresso Petals",
-    "NPX018":"Ruby Ribbon"
+    "NPX018":"Ruby Ribbon","NPF020":"Amber Mist","NOJ006":"Toffee Muse","NOJ007":"Cherry Glaze"
 }
 updated_mapping = dict(sku_prefix_to_name)
 
