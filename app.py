7c8b61a1d431567e3c14824615be0084eeb2f8b4
Chenhao Fan <146764940+ChenhaooooFan@users.noreply.github.com>
2025-12-26 14:14:06 -0800

    app.py

--- app.py
+++ app.py
@@ -38,14 +38,14 @@ sku_prefix_to_name = {
     "NOJ011":"Snowy Comet","NOX013":"Christmas Village","NOJ009":"Reindeer Glow","NIX002":"Golden Orchid", "NPX021":"Twinkle Pine",
     "NOF018":"Glacier Bloom","NOJ010":"Rosé Noir","NPX022":"Merry Charm", "NPJ014":"Snow Pixie","NPJ018":"Frost Ruby",
     "NPJ017":"Starlit Rift","NPF021":"Candy Cane","NPJ016":"Fairy Nectar","NPJ015":"Icy Viper","NOX014":"Taro Petal","NVT001":"Tool Kits",
-    "NF001":"Free Giveaway","NIF001":"Lilac Veil","NIF002":"Gingerbread","NOX015":"Glitter Doll","NOJ012":"Winery Flame","NOF021":"Velvet Ribbon","NPX024":"Rose Wine","NPX023":"Rosy Promise","NMF001":"Cherry Crush","NBX001":"Ballet Petal","NMF003":"Royal Treasure","NMF002":"Safari Princess"
+    "NF001":"Free Giveaway","NIF001":"Lilac Veil","NIF002":"Gingerbread","NOX015":"Glitter Doll","NOJ012":"Winery Flame","NOF021":"Velvet Ribbon","NPX024":"Rose Wine","NPX023":"Rosy Promise","NMF001":"Cherry Crush","NBX001":"Ballet Petal","NMF003":"Royal Treasure","NMF002":"Safari Princess","NOJ014":"Imperial Frost","NOJ013":"Midnight Denim"
 }
 updated_mapping = dict(sku_prefix_to_name)
 
 # 🆕 新款映射表，所有新款加到这，格式为："NOF018":"Glacier Bloom"
 new_sku_prefix = {
     "NPJ014":"Snow Pixie","NPJ018":"Frost Ruby","NPJ017":"Starlit Rift","NPF021":"Candy Cane",
-    "NPJ016":"Fairy Nectar","NPJ015":"Icy Viper","NOX014":"Taro Petal","NIF001":"Lilac Veil","NIF002":"Gingerbread","NOX015":"Glitter Doll","NOJ012":"Winery Flame","NOF021":"Velvet Ribbon","NBX001":"Ballet Petal","NMF003":"Royal Treasure"
+    "NPJ016":"Fairy Nectar","NPJ015":"Icy Viper","NOX014":"Taro Petal","NIF001":"Lilac Veil","NIF002":"Gingerbread","NOX015":"Glitter Doll","NOJ012":"Winery Flame","NOF021":"Velvet Ribbon","NBX001":"Ballet Petal","NMF003":"Royal Treasure","NOJ014":"Imperial Frost","NOJ013":"Midnight Denim"
 }
 
 # ---------- 小工具 ----------
