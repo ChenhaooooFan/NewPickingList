465540e5a0ef042773f2868e5cef910b81ea1b01
Chenhao Fan <146764940+ChenhaooooFan@users.noreply.github.com>
2026-01-08 16:24:36 -0800

    app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ updated_mapping = dict(sku_prefix_to_name)
 new_sku_prefix = {
     "NPJ014":"Snow Pixie","NPJ018":"Frost Ruby","NPJ017":"Starlit Rift","NPF021":"Candy Cane",
     "NPJ016":"Fairy Nectar","NPJ015":"Icy Viper","NOX014":"Taro Petal","NIF001":"Lilac Veil","NIF002":"Gingerbread","NOX015":"Glitter Doll","NOJ012":"Winery Flame","NOF021":"Velvet Ribbon","NBX001":"Ballet Petal","NMF003":"Royal Treasure","NOJ014":"Imperial Frost","NOJ013":"Midnight Denim","NOJ013":"Midnight Denim","NOJ014":"Imperial Frost",
-    "NPJ019":"Gothic Mist","NOJ015":"Sapphire Bloom",
+    "NOJ015":"Sapphire Bloom",
     "NPX025":"Cocoa Teddy","NVF001":"Golden Bloom","NBJ002":"Cherry Drop",
     "NOF022":"Aqua Reverie","NPF023":"Arctic Starlight"
 }
