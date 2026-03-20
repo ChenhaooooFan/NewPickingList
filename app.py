21bd02da06fd6babffc1f630367949863301a689
Chenhao Fan <146764940+ChenhaooooFan@users.noreply.github.com>
2026-03-20 13:43:23 -0700

    app.py
    Updated the product mapping with new items and corrected the format.

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ sku_prefix_to_name = {
     "NPJ019":"Gothic Mist","NOJ015":"Sapphire Bloom",
     "NPX025":"Cocoa Teddy","NVF001":"Golden Bloom","NBJ002":"Cherry Drop",
     "NOF022":"Aqua Reverie","NPF023":"Arctic Starlight","NDJ001":"Snow Knit",
-    "NOX016":"Cherry Ribbon","NOX017":"Ruby Bow","NMF004":"Lavender Bloom","NDX002":"Cloudy Knit","NMJ003":"Gothic Rose","NOF025":"Cherry Romance","NMJ001":"Milky Cloud","NMX001":"Petal Muse","NOF024":"Floral Muse","NVX001":"Sakura Macaron","NVF002":"Dreamy Bloom","NOJ017":"Floral Garden","NOJ016":"Jade Blossom","NVX002":"Pastel Bloom","NPF023":"Fairy Garden","NBJ001":"Stone Petal","NOF027":"Acai Bloom","NPJ021":"Champagne Blossom","NPJ020":"Citrus Daisy","NOJ018":"Ribbon Lily","NVF005":"Dreamy Sakura","NDX003":"Meadow Petals","NOX018":"Strawberry Kiss","NOJ020":"Raibow Bloom"
+    "NOX016":"Cherry Ribbon","NOX017":"Ruby Bow","NMF004":"Lavender Bloom","NDX002":"Cloudy Knit","NMJ003":"Gothic Rose","NOF025":"Cherry Romance","NMJ001":"Milky Cloud","NMX001":"Petal Muse","NOF024":"Floral Muse","NVX001":"Sakura Macaron","NVF002":"Dreamy Bloom","NOJ017":"Floral Garden","NOJ016":"Jade Blossom","NVX002":"Pastel Bloom","NPF023":"Fairy Garden","NBJ001":"Stone Petal","NOF027":"Acai Bloom","NPJ021":"Champagne Blossom","NPJ020":"Citrus Daisy","NOJ018":"Ribbon Lily","NVF005":"Dreamy Sakura","NDX003":"Meadow Petals","NOX018":"Strawberry Kiss","NOJ020":"Raibow Bloom","NPF026":"Seaside Sundae","NVJ001":"Prism Aura"
 }
 updated_mapping = dict(sku_prefix_to_name)
 
