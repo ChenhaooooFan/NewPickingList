64128029fa0b55e68670cc387f0ac2d77fb78dbf
Chenhao Fan <146764940+ChenhaooooFan@users.noreply.github.com>
2026-06-29 13:21:32 -0700

    app.py

--- app.py
+++ app.py
@@ -571,7 +571,7 @@ sku_prefix_to_name = {
     "NPX025":"Cocoa Teddy","NVF001":"Golden Bloom","NBJ002":"Cherry Drop",
     "NOF022":"Aqua Reverie","NDJ001":"Snow Knit","NOF023":"Arctic Starlight",
     "NOX016":"Cherry Ribbon","NOX017":"Ruby Bow","NMF004":"Lavender Bloom","NDX002":"Cloudy Knit","NMJ003":"Gothic Rose",
-    "NOF025":"Cherry Romance","NMJ001":"Milky Cloud",
+    "NOF025":"Cherry Romance","NMJ001":"Milky Cloud","NOX028":"Rose Champagne","NOF040":"Champagne Shell","NOF041":"Blooming Malibu","NOF042":"Rosy Puff",
     "NMX001":"Petal Muse","NOF024":"Floral Muse","NVX001":"Sakura Macaron","NVF002":"Dreamy Bloom","NOJ017":"Floral Garden",
     "NOJ016":"Jade Blossom","NVX002":"Pastel Bloom","NVF008":"Glazed Ballet","NWF008":"Waikiki Blossom","NWF009":"Petal French",
     "NPF023":"Fairy Garden","NBJ001":"Stone Petal","NOF027":"Acai Bloom","NPJ021":"Champagne Blossom","NPJ020":"Citrus Daisy",
